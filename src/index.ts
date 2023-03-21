9ec733ab938e4436f754a7f9a6f801f69f210f75
Chris Navarro <cnavarro@dovetailsoftware.com>
2023-03-21 12:06:33 -0500

    some changes

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,11 @@ import { createServiceModuleWithOptions, IServiceModule } from '@aesop-fables/co
 import { DynamoDB } from 'aws-sdk';
 import { DynamoFactory } from './DynamoFactory';
 import { DynamoServices } from './DynamoServices';
-import { IDynamoOperation } from './IDynamoOperation';
 import { DynamoService, IDynamoService } from './IDynamoService';
 
-export { IDynamoOperation, IDynamoService, DynamoService, DynamoFactory, DynamoServices };
-
-export interface UseDynamoConfiguration {
-  core?: DynamoDB.Types.ClientConfiguration;
-}
-
-export const useDynamo: (options: UseDynamoConfiguration) => IServiceModule =
-  createServiceModuleWithOptions<UseDynamoConfiguration>('useDynamo', (services, options) => {
-    const client = DynamoFactory.createFullClient(options.core);
+export const useDynamo: (options: DynamoDB.Types.ClientConfiguration) => IServiceModule =
+  createServiceModuleWithOptions<DynamoDB.Types.ClientConfiguration>('useDynamo', (services, options) => {
+    const client = DynamoFactory.createFullClient(options);
     services.register<IDynamoService>(DynamoServices.Service, (container) => new DynamoService(container));
     services.register<DynamoDB>(DynamoServices.Client, client);
   });
